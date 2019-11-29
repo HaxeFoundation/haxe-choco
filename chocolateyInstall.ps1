1cf46683c3b31e08764894e4c89139dfe46b0630
Andy Li <andy@onthewings.net>
2019-11-30 07:08:28 +0800

    haxe 4.0.3

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -4,8 +4,8 @@ Install-ChocolateyZipPackage `
     -PackageName 'haxe' `
     -UnzipLocation "$packDir" `
     -Url "$packDir\haxe-*-win.zip" `
-    -Checksum '7f593bbbbc5be5f0344dc567e7aa69f39a2aaf661f2a088ed349f12849d06001' `
+    -Checksum 'aae6667d94ff45603df0757bea0762c871985f66e5d4755a039b7a4efbf4d70d' `
     -ChecksumType 'sha256' `
     -Url64bit "$packDir\haxe-*-win64.zip" `
-    -Checksum64 '787cf7d67da5922c9aaf79c0c4b03c3d7350c522915884b70a27e774dac1560f' `
+    -Checksum64 '80cff2d8fbfd46a5be79413411554eea4de81f5c3dbfe780546f59e12f536c24' `
     -ChecksumType64 'sha256'
