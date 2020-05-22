67650ef36e86181e7c302174343eb93b5c0540a2
Andy Li <andy@onthewings.net>
2020-05-22 15:35:25 +0800

    haxe 4.1.0

--- chocolateyInstall.ps1
+++ chocolateyInstall.ps1
@@ -3,9 +3,9 @@ $packDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 Install-ChocolateyZipPackage `
     -PackageName 'haxe' `
     -UnzipLocation "$packDir" `
-    -Url "$packDir\haxe-*-win.zip" `
-    -Checksum '72c6015616bfec77c74b6402318b5cca667ef0010077d7db5b0210fa529ca7eb' `
+    -Url "$packDir\haxe-4.1.0-win.zip" `
+    -Checksum '7c38c50b6ec76e0dabbbbaa6a3c563fcc6dd130b929968ddef36fef30dbe979e' `
     -ChecksumType 'sha256' `
-    -Url64bit "$packDir\haxe-*-win64.zip" `
-    -Checksum64 '93130ae2b1083efbcd9b8911afe2ba00d5af995f016149fd7ec629fa439c6120' `
+    -Url64bit "$packDir\haxe-4.1.0-win64.zip" `
+    -Checksum64 'ed5d323a565cc9a51891f9a79fdb516a87110d4f36fa6b334d000690d3a1cfb1' `
     -ChecksumType64 'sha256'
